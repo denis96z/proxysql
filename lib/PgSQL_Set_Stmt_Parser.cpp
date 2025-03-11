94399faaf942c1b9bd68768d2bb99292580a0877
Rahim Kanji <rahim.kanji@outlook.com>
2025-03-11 14:15:13 +0500

    Added 1 and 0 in bool validation

--- lib/PgSQL_Set_Stmt_Parser.cpp
+++ lib/PgSQL_Set_Stmt_Parser.cpp
@@ -74,7 +74,7 @@ void PgSQL_Set_Stmt_Parser::generateRE_parse1v2() {
 	// Function Call: Check if Group 3 is populated.
 	// Literal: Check if Group 4 is populated.
 	//const std::string pattern = "(?:(SESSION|LOCAL)\\s+)?((?:\\S+(?:\\s+\\S+)*?))(?:\\s+(?:TO|=)\\s+|\\s+)(?:(\\w+\\s*\\([^)]*\\))|((?:'(?:''|[^'])*'|-?\\d+(?:\\.\\d+)?(?:[eE][+-]?\\d+)?|t|true|f|false|on|off|default|\\S+)))\\s*;?";
-	const std::string pattern = "(?:(SESSION|LOCAL)\\s+)?((?:\\S+(?:\\s+\\S+)*?))(?:\\s*(?:TO|=)\\s*|\\s+)(?:(\\w+\\s*\\([^)]*\\))|((?:'(?:''|[^'])*'|-?\\d+(?:\\.\\d+)?(?:[eE][+-]?\\d+)?|true|t|false|f|on|off|default|\\S+)))\\s*;?";
+	const std::string pattern = "(?:(SESSION|LOCAL)\\s+)?((?:\\S+(?:\\s+\\S+)*?))(?:\\s*(?:TO|=)\\s*|\\s+)(?:(\\w+\\s*\\([^)]*\\))|((?:'(?:''|[^'])*'|-?\\d+(?:\\.\\d+)?(?:[eE][+-]?\\d+)?|true|t|1|yes|false|f|0|no|on|off|default|\\S+)))\\s*;?";
 
 #ifdef DEBUG
 VALGRIND_DISABLE_ERROR_REPORTING;
